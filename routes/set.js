91e82da30d05e0ebbaf34bf73964aca5a1c0558d
abbas0001 <84105420+abbas0001@users.noreply.github.com>
2023-02-20 16:29:13 +0330

    set api progress

--- routes/set.js
+++ routes/set.js
@@ -25,7 +25,49 @@ router.post('/set', async (req, res) => {
                 const user = await userModel.find({ user: input.user });
                 if (user.length != 0) {
                     const clip = await clipModel.find({ user: input.user });
-                    console.log(clip);
+                    const updatedData = { user: input.user };
+                    updatedData.clip1 = clip.clip1;
+                    updatedData.clip2 = clip.clip2;
+                    updatedData.clip3 = clip.clip3;
+                    updatedData.clip4 = clip.clip4;
+                    updatedData.clip5 = clip.clip5;
+                    updatedData.clip6 = clip.clip6;
+                    updatedData.clip7 = clip.clip7;
+                    updatedData.clip8 = clip.clip8;
+                    updatedData.clip9 = clip.clip9;
+                    updatedData.clip10 = clip.clip10;
+                    if (clipResult[0] == 'clip1') {
+                        updatedData.clip1 = input.clip1;
+                    }
+                    if (clipResult[0] == 'clip2') {
+                        updatedData.clip2 = input.clip2;
+                    }
+                    if (clipResult[0] == 'clip3') {
+                        updatedData.clip3 = input.clip3;
+                    }
+                    if (clipResult[0] == 'clip4') {
+                        updatedData.clip4 = input.clip4;
+                    }
+                    if (clipResult[0] == 'clip5') {
+                        updatedData.clip5 = input.clip5;
+                    }
+                    if (clipResult[0] == 'clip6') {
+                        updatedData.clip6 = input.clip6;
+                    }
+                    if (clipResult[0] == 'clip7') {
+                        updatedData.clip7 = input.clip7;
+                    }
+                    if (clipResult[0] == 'clip8') {
+                        updatedData.clip8 = input.clip8;
+                    }
+                    if (clipResult[0] == 'clip9') {
+                        updatedData.clip9 = input.clip9;
+                    }
+                    if (clipResult[0] == 'clip10') {
+                        updatedData.clip10 = input.clip10;
+                    }
+                    console.log(updatedData);
+                    res.send({ message: 'clip saved' });
                 } else {
                     res.send({ error: 'user not found' });
                 }
